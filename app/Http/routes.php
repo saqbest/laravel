bd6f4259cdd51aa53bfde60ce617cae04b0c127e
saqbest <saqoxachatryan1988@gmail.com>
2016-01-25 13:57:01 +0400

    Add login end registration

---
+++ app/Http/routes.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('home',function(){
+    if(Auth::guest()){
+        return Redirect::to('auth/login');
+
+    }
+    else{return 'Welcome  '.Auth::user()->email.'.';}
+
+});
+Route::get('user/{id}',function($id){
+   $user=App\User::find($id);
+    echo 'User with id of'.$id.'has en email of:'.$user->email;
+});
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
